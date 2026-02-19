e16f99aeaa2420327dea5e860a6590f072175302
MaykonBat <mab.maykon@gmail.com>
2026-02-19 16:14:53 -0300

    first version of blockchain and CerberusPay contract deployment

--- blockchain/contracts/CerberusPay.sol
+++ blockchain/contracts/CerberusPay.sol
@@ -4,6 +4,70 @@ pragma solidity ^0.8.23;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract CerberusPay {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+contract CerberusPay is Ownable, Pausable, ReentrancyGuard {
+    IERC20 public acceptedToken;
+
+    uint public monthlyAmount = 0.001 ether;
+    uint private constant thirtyDaysInSeconds = 30 * 24 * 60 * 60;
+
+    mapping(address => uint) public payments; //customer => next payment
+    address[] public customers;
+
+    event Paid(address indexed customer, uint date, uint amount);
+
+    constructor() Ownable(msg.sender) {
+        acceptedToken = IERC20(0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14); //SEPOLIA WETH
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function getCustomers() external view returns (address[] memory) {
+        return customers;
+    }
+
+    function setAcceptedToken(address tokenAddress) external onlyOwner {
+        acceptedToken = IERC20(tokenAddress);
+    }
+
+    function setMonthlyAmount(uint newAmount) external onlyOwner {
+        monthlyAmount = newAmount;
+    }
+
+    function withdraw(uint amount) external onlyOwner nonReentrant {
+        uint balance = acceptedToken.balanceOf(address(this));
+        require(balance >= amount, "Insufficient balance");
+        acceptedToken.transfer(owner(), amount);
+    }
+
+    function pay(address customer) external onlyOwner whenNotPaused {
+        bool thirtyDaysHavePassed = payments[customer] <= block.timestamp;
+        bool firstPayment = payments[customer] == 0;
+        bool hasAmount = acceptedToken.balanceOf(customer) >= monthlyAmount;
+        bool hasAllowance = acceptedToken.allowance(customer, address(this)) >= monthlyAmount;
+        bool timeToPay = thirtyDaysHavePassed || firstPayment;
+
+        if (!timeToPay) return;
+
+        if (!hasAmount || !hasAllowance)
+            revert("Insufficient balance or allowance");
+
+        acceptedToken.transferFrom(customer, address(this), monthlyAmount);
+
+        if (firstPayment) customers.push(customer);
+
+        payments[customer] = block.timestamp + thirtyDaysInSeconds;
+
+        emit Paid(customer, block.timestamp, monthlyAmount);
+    }
 }
--- blockchain/test/CerberusPay.test.ts
+++ blockchain/test/CerberusPay.test.ts
@@ -9,17 +9,97 @@ describe("CerberusPay Tests", function () {
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
 
+    const CerberusCoin = await ethers.getContractFactory("CerberusCoin");
+    const cerberusCoin = await CerberusCoin.deploy();
+    await cerberusCoin.waitForDeployment();
+
     const CerberusPay = await ethers.getContractFactory("CerberusPay");
     const cerberusPay = await CerberusPay.deploy();
+    await cerberusPay.waitForDeployment();
 
-    return { cerberusPay, owner, otherAccount };
+    const cerberusAddress = cerberusPay.target;
+
+    await cerberusPay.setAcceptedToken(cerberusCoin.target);
+
+    await cerberusCoin.mint(otherAccount.address, ethers.parseEther("1"));
+
+    return { cerberusPay, cerberusCoin, cerberusAddress, owner, otherAccount };
   }
 
-  it("Should ", async function () {
-    const { cerberusPay, owner, otherAccount } = await loadFixture(
-      deployFixture,
-    );
+  it("Should do first payment", async function () {
+    const { cerberusPay, cerberusCoin, cerberusAddress, otherAccount } = await loadFixture(deployFixture);
 
-    //expect(await lock.unlockTime()).to.equal(unlockTime);
+    const instance = cerberusCoin.connect(otherAccount);
+    await instance.approve(cerberusAddress, ethers.parseEther("0.01"));
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.emit(cerberusPay, "Paid");
   });
+
+  it("Should NOT do first payment", async function () {
+    const { cerberusPay, otherAccount } = await loadFixture(deployFixture);
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance or allowance");
+  });
+
+  it("Should do second payment", async function () {
+    const { cerberusPay, cerberusCoin, cerberusAddress, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = cerberusCoin.connect(otherAccount);
+    await instance.approve(cerberusAddress, ethers.parseEther("0.01"));
+
+    await cerberusPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60); // Increase time by 31 days
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.emit(cerberusPay, "Paid");
+  });
+
+  it("Should NOT do second payment", async function () {
+    const { cerberusPay, cerberusCoin, cerberusAddress, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = cerberusCoin.connect(otherAccount);
+    await instance.approve(cerberusAddress, ethers.parseEther("0.01"));
+
+    await cerberusPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60); // Increase time by 31 days
+
+    await instance.approve(cerberusAddress, ethers.parseEther("0.00001"));
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance or allowance");
+  });
+
+  it("Should do second payment after failure", async function () {
+    const { cerberusPay, cerberusCoin, cerberusAddress, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = cerberusCoin.connect(otherAccount);
+    await instance.approve(cerberusAddress, ethers.parseEther("0.01"));
+
+    await cerberusPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60); // Increase time by 31 days
+
+    await instance.approve(cerberusAddress, ethers.parseEther("0.00001"));
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance or allowance");
+
+    await instance.approve(cerberusAddress, ethers.parseEther("1"));
+
+    await expect(cerberusPay.pay(otherAccount.address)).to.emit(cerberusPay, "Paid");
+  });
+
+  it("Should withdraw", async function () {
+    const { cerberusPay, cerberusCoin, cerberusAddress, otherAccount } = await loadFixture(deployFixture);
+
+    //test payment
+    const value = await cerberusPay.monthlyAmount();
+    const instance = cerberusCoin.connect(otherAccount);
+    await instance.approve(cerberusAddress, value);
+    await cerberusPay.pay(otherAccount.address);
+    expect(await instance.balanceOf(cerberusAddress)).to.be.equals(value);
+
+    await cerberusPay.withdraw(value);
+    expect(await instance.balanceOf(cerberusAddress)).to.be.equals(0);
+  });
+
 });
